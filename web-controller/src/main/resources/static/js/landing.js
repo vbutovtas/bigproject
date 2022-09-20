51d526e957b16c160f0be6812c923caef39cc52b
Vitali Butovtas <vbutovtas@mail.ru>
2022-09-21 01:42:34 +0300

    Added carousel. Redesigned submit request form

--- web-controller/src/main/resources/static/js/landing.js
+++ web-controller/src/main/resources/static/js/landing.js
@@ -1,66 +1,19 @@
 $(document).ready(function(){
 
-	// Scrollspy initiation
-	$('body').scrollspy({
-		target: '#scroll-spy',
-		offset: 70
-	});
-
-	// On render, adjust body padding to ensure last Scroll target can reach top of screen
-	var height = $('#howto').innerHeight();
-	var windowHeight = $(window).height();
-	var navHeight = $('nav.navbar').innerHeight();
-	var siblingHeight = $('#howto').nextAll().innerHeight();
-
-
-	if(height < windowHeight){
-		$('body').css("padding-bottom", windowHeight-navHeight-height-siblingHeight + "px");
-	}
-
-	// On window resize, adjust body padding to ensure last Scroll target can reach top of screen
-	$(window).resize(function(event){
-		var height = $('#howto').innerHeight();
-		var windowHeight = $(window).height();
-		var navHeight = $('nav.navbar').innerHeight();
-		var siblingHeight = $('#howto').nextAll().innerHeight();
-
-
-		if(height < windowHeight){
-			$('body').css("padding-bottom", windowHeight-navHeight-height-siblingHeight + "px");
-		}
-	});
-
 	$('nav.navbar a, .scrollTop').click(function(event){
-		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== "") {
-			// Prevent default anchor click behavior
 			event.preventDefault();
-
-			// Store hash (#)
 			var hash = this.hash;
-
-			// Ensure no section has relevant class
 			$('section').removeClass("focus");
-
-			// Add class to target
 			$(hash).addClass("focus");
-
-			// Remove thy class after timeout
 			setTimeout(function(){
 				$(hash).removeClass("focus");
 			}, 2000);
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top - 69
 			}, 600, function(){
-
-				// Add hash (#) to URL when done scrolling (default click behavior)
 				window.location.hash = hash;
 			});
-
-			// Collapse Navbar for mobile view
 			$(".navbar-collapse").collapse('hide');
 		}
 
@@ -76,4 +29,37 @@ $(document).ready(function(){
 		}
 
 	});
+});
+$(function(){
+  $('.carousel-item').eq(0).addClass('active');
+  var total = $('.carousel-item').length;
+  var current = 0;
+  $('#moveRight').on('click', function(){
+    var next=current;
+    current= current+1;
+    setSlide(next, current);
+  });
+  $('#moveLeft').on('click', function(){
+    var prev=current;
+    current = current- 1;
+    setSlide(prev, current);
+  });
+  function setSlide(prev, next){
+    var slide= current;
+    if(next>total-1){
+     slide=0;
+      current=0;
+    }
+    if(next<0){
+      slide=total - 1;
+      current=total - 1;
+    }
+           $('.carousel-item').eq(prev).removeClass('active');
+           $('.carousel-item').eq(slide).addClass('active');
+      setTimeout(function(){
+
+      },800);
+    console.log('current '+current);
+    console.log('prev '+prev);
+  }
 });
